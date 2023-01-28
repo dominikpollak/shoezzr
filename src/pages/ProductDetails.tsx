69a565a4a3ec7abc37c915f3e76ede1eeb25d7bf
dominikpollak <dominikpollak17@gmail.com>
2023-01-28 11:14:01 +0100

    add: product detail color list

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import shoes from '../shoes.json';
 
@@ -6,6 +6,7 @@ export default function ProductDetails() {
   const { slug } = useParams();
   const matchedShoe = shoes.find((shoe) => shoe._id === slug);
   const nameRef = useRef<HTMLHeadingElement>(null);
+  //   const [colorSelection, setColorSelection] = useState<string>('');
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let releaseDate = matchedShoe?.releaseDate.split('-').reverse().join('.');
   if (releaseDate === '') releaseDate = 'unknown';
@@ -13,6 +14,8 @@ export default function ProductDetails() {
   if (description === '' || description === '\n')
     description =
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur dolorem blanditiis nobis molestias cum odit natus quisquam sit voluptatem a facere maiores, repudiandae libero dolorum animi laboriosam necessitatibus quam temporibus.';
+  const colorArray = matchedShoe?.colorway.split('/');
+  const colorList = Array.from(new Set(colorArray));
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -50,24 +53,37 @@ export default function ProductDetails() {
   return (
     <div className="h-screen w-screen font-main flex justify-center mt-[2rem] text-white ">
       <main className="w-[90%] h-[80%] bg-white rounded-lg">
+        <aside className="float-left w-[50%] h-full p-2 flex justify-center items-center">
+          <img className="w-[85%]" src={matchedShoe?.thumbnail} alt="" />
+        </aside>
         <aside className="w-[50%] h-full bg-black/[0.9] float-right p-2">
           <h1
             ref={nameRef}
-            className="text-center rounded-lg text-[2rem] uppercase text-black bg-orange-600 break-words"
+            className="text-center rounded-lg text-[2rem] font-black uppercase text-black bg-orange-600 break-words"
           >
             {matchedShoe?.shoeName}
           </h1>
-          <div className="p-4">
-            <span>Released:</span>
-            <span> {releaseDate}</span>
-            <h2 className="mt-4 text-black bg-white w-min px-1 mb-2 text-[1.1rem]">
+          <section className="p-4">
+            <div className="text-center py-2">Released: {releaseDate}</div>
+            <h2 className="mt-4 text-black bg-white w-max px-1 mb-2 text-[1.1rem]">
               Description:
             </h2>
-            <article>{description}</article>
-          </div>
-        </aside>
-        <aside className="left-0 w-[50%] h-full p-2 flex  justify-center items-center">
-          <img className="w-[85%]" src={matchedShoe?.thumbnail} alt="" />
+            <article className="bg-orange-600/[0.1] p-3 border-[1px] border-orange-600">
+              {description}
+            </article>
+            <h2 className="mt-8 text-black bg-white w-max px-1 mb-2 text-[1.1rem]">
+              Available colors:
+            </h2>
+            <ul>
+              {colorList.map((color, index) => {
+                return (
+                  <li key={index} className="">
+                    <div className="border-[1px] w-max px-1">{color}</div>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
         </aside>
       </main>
     </div>
