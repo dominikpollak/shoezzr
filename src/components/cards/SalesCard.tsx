126ac926aa4c3b494fbd2524713adb8a903483ec
dominikpollak <dominikpollak17@gmail.com>
2023-03-16 20:17:09 +0100

    Improvement: Sales card look

--- src/components/cards/SalesCard.tsx
+++ src/components/cards/SalesCard.tsx
@@ -5,9 +5,9 @@ const SalesCard = () => {
   return (
     <Link
       to="/sales"
-      className="group relative h-[45%] w-[80%] rounded-sm bg-gradient-to-br from-orange-300 via-orange-700 to-slate-900 shadow-md ring-2 ring-white duration-500 hover:ring-4 sm:w-[70%] lg:w-[40%]"
+      className="group relative h-[45%] w-[80%] rounded-sm bg-black shadow-md ring-2 ring-yellow-500 duration-500 hover:ring-4 sm:w-[70%] lg:w-[40%]"
     >
-      <div className="flex h-full w-full items-center justify-center font-main text-[1.6rem] font-black tracking-[0.2rem] text-white duration-300 group-hover:tracking-[0.3rem] sm:text-[2rem]">
+      <div className="flex h-full w-full items-center justify-center bg-gradient-to-br from-yellow-300 to-yellow-600 bg-clip-text font-main text-[1.6rem] font-black tracking-[0.2rem] text-transparent duration-300 group-hover:tracking-[0.3rem] sm:text-[2rem]">
         SPECIAL OFFERS
       </div>
       <div className="absolute bottom-0 right-0 flex h-[70%] w-[18%] translate-x-[50%] translate-y-[50%] rotate-[-12deg] items-center justify-center rounded-md border-4 border-black bg-gradient-to-b from-yellow-300 to-yellow-600 font-main text-[2.6rem] duration-300 group-hover:scale-105 sm:h-[75%] sm:w-[11%] sm:text-[3rem]">
